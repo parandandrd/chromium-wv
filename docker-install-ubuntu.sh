4928f113d660b56501a9fc8d57bc2f0b684ad095
parandandrd <one.paranoid.android@mac.com>
2021-04-19 16:37:23 -0500

    Update and rename install.sh to docker-install-ubuntu.sh

---
+++ docker-install-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker $USER
+
+newgrp docker
